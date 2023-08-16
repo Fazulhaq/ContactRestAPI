5e3ce4aee00abd15e6ff0874934957eb35e83cdc
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2023-08-16 16:56:36 +0430

    OpenAPI Documentations has been added into the Controller class APIs. Continue

--- src/main/java/com/mcit/contacts/config/OpenApiConfig.java
+++ src/main/java/com/mcit/contacts/config/OpenApiConfig.java
@@ -3,13 +3,15 @@ package com.mcit.contacts.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI()
                 .info(new Info()
-                        .title("Contact Documentations")
+                        .title("Contact OpenAPI Documentations")
                         .description("This documentation is used for better use of APIs")
                         .version("1.0"));
     }
